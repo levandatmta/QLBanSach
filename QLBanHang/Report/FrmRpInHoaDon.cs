85f0e90df8d2b59fe4de3e92726c32fdb2342f88
viet1997 <37035291+viet1997@users.noreply.github.com>
2018-09-20 15:49:52 +0700

    viet 3

--- QLBanHang/Report/FrmRpInHoaDon.cs
+++ QLBanHang/Report/FrmRpInHoaDon.cs
@@ -25,36 +25,8 @@ namespace QLBanHang.Report
             Service.DBService.Reload();
             hd = z;
         }
-        #endregion
 
-        #region LoadForm
-        private void FrmRpInHoaDon_Load(object sender, EventArgs e)
-        {
-
-            int i = 0;
-            ChiTietBanBindingSource.DataSource = (
-                                                    from ctb in db.CHITIETXUATs.Where(p => p.HOADONBANID == hd.ID).ToList()
-                                                    from sach in db.SACHes.Where(p => p.ID == ctb.SACHID).ToList()
-                                                    select new
-                                                    {
-                                                        STT = ++i,
-                                                        Sach = sach.TEN,
-                                                        SoLuong = ctb.SOLUONG,
-                                                        GiaBan = ((int) sach.GIABAN).ToString("N0"),
-                                                        ThanhTien = ((int) ctb.THANHTIEN).ToString("N0")
-                                                    }
-                                                  ).ToList();
-
-            // add parameter
-            ReportParameter[] listPara = new ReportParameter[]{
-                    new ReportParameter("NhanVien", db.NHANVIENs.Where(z=>z.ID == hd.NHANVIENID).FirstOrDefault().TEN),
-                    new ReportParameter("ThoiGian", ((DateTime) hd.NGAY).ToString("HH:mm dd/MM/yyyy")),
-                    new ReportParameter("TongTien", ((int) hd.TONGTIEN).ToString("N0"))
-            };
-
-            this.reportViewer1.LocalReport.SetParameters(listPara);
-            this.reportViewer1.RefreshReport();
-        }
+      
         #endregion
     }
 }
